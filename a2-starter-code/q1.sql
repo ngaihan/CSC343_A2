b58f518a77f76b5db6a884598cd7ad3af3d543c9
Ngai Han Lin <ngaihan@teach.cs.toronto.edu>
2023-11-01 13:06:40 -0400

    q1 finnished writing not tested

--- a2-starter-code/q1.sql
+++ a2-starter-code/q1.sql
@@ -16,12 +16,61 @@ CREATE TABLE q1 (
 -- Do this for each of the views that define your intermediate steps.
 -- (But give them better names!) The IF EXISTS avoids generating an error
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
-
+DROP VIEW IF EXISTS 80to100 CASCADE;
+DROP VIEW IF EXISTS 60to79 CASCADE;
+DROP VIEW IF EXISTS 50to59 CASCADE;
+DROP VIEW IF EXISTS 0to49 CASCADE;
+DROP VIEW IF EXISTS averageMark CASCADE;
 
 -- Define views for your intermediate steps here:
-CREATE VIEW intermediate_step AS ... ;
+-- Find the count of each different grade bracket for each assignment
+CREATE VIEW 80to100 AS 
+SELECT assignment_id, count(*) AS num_80_100
+FROM assignment JOIN assignmentgroup USING (assignment_id)
+				JOIN grade USING (group_id)
+WHERE grade >= 80
+GROUP BY assignment_id
+;
 
+CREATE VIEW 60to79 AS 
+SELECT assignment_id, count(*) AS num_60_79
+FROM assignment JOIN assignmentgroup USING (assignment_id)
+				JOIN grade USING (group_id)
+WHERE grade >= 60 AND grade < 80
+GROUP BY assignment_id
+;
+
+CREATE VIEW 50to59 AS 
+SELECT assignment_id, count(*) AS num_50_59
+FROM assignment JOIN assignmentgroup USING (assignment_id)
+				JOIN grade USING (group_id)
+WHERE grade >= 50 AND grade < 60
+GROUP BY assignment_id
+;
+
+CREATE VIEW 0to49 AS num_0_49
+SELECT assignment_id, count(*)
+FROM assignment JOIN assignmentgroup USING (assignment_id)
+				JOIN grade USING (group_id)
+WHERE grade < 50
+GROUP BY assignment_id
+;
+
+CREATE VIEW averageMark AS average_mark_percent
+SELECT assignment_id, avg(grade) AS 
+FROM assignment JOIN assignmentgroup USING (assignment_id)
+				JOIN grade USING (group_id)
+GROUP BY assignment_id
+;
+
+CREATE VIEW solution AS
+SELECT assignment_id, average_mark_percent, num_80_100, num_60_79, num_50_59, num_0_49
+FROM averageMark JOIN 0to49 USING (assignment_id)
+			     JOIN 50to59 USING (assignment_id)
+				 JOIN 60to79 USING (assignment_id)
+				 JOIN 80to100 USING (assignment_id)
+;
 
 -- Your query that answers the question goes below the "insert into" line:
 INSERT INTO q1
+solution;
