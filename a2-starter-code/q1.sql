585467bfc34845640bd85579d8851fe0b0426d37
Ngai Han Lin <ngaihan@teach.cs.toronto.edu>
2023-11-04 16:49:26 -0400

    q1 fully functional and tested.

--- a2-starter-code/q1.sql
+++ a2-starter-code/q1.sql
@@ -16,75 +16,74 @@ CREATE TABLE q1 (
 -- Do this for each of the views that define your intermediate steps.
 -- (But give them better names!) The IF EXISTS avoids generating an error
 -- the first time this file is imported.
-DROP VIEW IF EXISTS 80to100 CASCADE;
-DROP VIEW IF EXISTS 60to79 CASCADE;
-DROP VIEW IF EXISTS 50to59 CASCADE;
-DROP VIEW IF EXISTS 0to49 CASCADE;
+DROP VIEW IF EXISTS grade80to100 CASCADE;
+DROP VIEW IF EXISTS grade60to79 CASCADE;
+DROP VIEW IF EXISTS grade50to59 CASCADE;
+DROP VIEW IF EXISTS grade0to49 CASCADE;
 DROP VIEW IF EXISTS averageMark CASCADE;
+DROP VIEW IF EXISTS percentMark CASCADE;
+DROP VIEW IF EXISTS rubricWeight CASCADE;
 
 -- Define views for your intermediate steps here:
--- Find the grade by using rubric and grade
-CREATE VIEW rubricGrades AS
-SELECT assignment_id, group_id, grade, out_of, weight
-FROM assignmentgroup JOIN grade USING (group_id)
-					 JOIN rubricitem USING (rubric_id)
+-- Total weight of each assignment for calculation of the total percentage mark
+CREATE VIEW rubricWeight AS
+SELECT assignment_id, sum(weight) AS total_weight
+FROM rubricitem
+GROUP BY assignment_id
 ;
 
--- Update the table so that it has the individual weight of each of the assignment items
-UPDATE rubricGrades
-SET grade = grade / out_of * weight * 100
-;
-
--- Aggregates each of the weighted rubric items and aggregates them by assignment, giving the total grade
-CREATE VIEW groupGrade AS
-SELECT assignment_id, group_id, sum(grade)
-FROM rubricGrades
-GROUP BY assignment_id, group_id
+-- The calculated percentage mark for each group and that shows up in the results table
+CREATE VIEW percentMark AS
+SELECT assignment_id, group_id, (mark/total_weight)*100 AS percent_mark
+FROM rubricWeight JOIN assignmentgroup USING (assignment_id)
+				  JOIN result USING (group_id)
 ;
 
 -- Find the count of each different grade bracket for each assignment
-CREATE VIEW 80to100 AS 
+CREATE VIEW grade80to100 AS 
 SELECT assignment_id, count(*) AS num_80_100
-FROM groupGrade
-WHERE grade >= 80
+FROM percentMark
+WHERE percent_mark >= 80
 GROUP BY assignment_id
 ;
 
-CREATE VIEW 60to79 AS 
+CREATE VIEW grade60to79 AS 
 SELECT assignment_id, count(*) AS num_60_79
-FROM groupGrade
-WHERE grade >= 60 AND grade < 80
+FROM percentMark
+WHERE percent_mark >= 60 AND percent_mark < 80
 GROUP BY assignment_id
 ;
 
-CREATE VIEW 50to59 AS 
+CREATE VIEW grade50to59 AS 
 SELECT assignment_id, count(*) AS num_50_59
-FROM groupGrade
-WHERE grade >= 50 AND grade < 60
+FROM percentMark
+WHERE percent_mark >= 50 AND percent_mark < 60
 GROUP BY assignment_id
 ;
 
-CREATE VIEW 0to49 AS num_0_49
-SELECT assignment_id, count(*)
-FROM groupGrade
-WHERE grade < 50
+CREATE VIEW grade0to49 AS
+SELECT assignment_id, count(*) AS num_0_49
+FROM percentMark
+WHERE percent_mark < 50
 GROUP BY assignment_id
 ;
 
-CREATE VIEW averageMark AS average_mark_percent
-SELECT assignment_id, avg(grade) AS 
-FROM groupGrade
+CREATE VIEW averageMark AS 
+SELECT assignment_id, avg(percent_mark) AS average_mark_percent
+FROM percentMark
 GROUP BY assignment_id
 ;
 
 CREATE VIEW solution AS
 SELECT assignment_id, average_mark_percent, num_80_100, num_60_79, num_50_59, num_0_49
-FROM averageMark JOIN 0to49 USING (assignment_id)
-			     JOIN 50to59 USING (assignment_id)
-				 JOIN 60to79 USING (assignment_id)
-				 JOIN 80to100 USING (assignment_id)
+FROM averageMark LEFT JOIN grade0to49 USING (assignment_id)
+			     LEFT JOIN grade50to59 USING (assignment_id)
+				 LEFT JOIN grade60to79 USING (assignment_id)
+				 LEFT JOIN grade80to100 USING (assignment_id)
 ;
 
 -- Your query that answers the question goes below the "insert into" line:
-INSERT INTO q1
-solution;
+INSERT INTO q1(assignment_id, average_mark_percent, num_80_100, num_60_79, num_50_59, num_0_49)
+SELECT assignment_id, COALESCE(average_mark_percent,0), COALESCE(num_80_100,0), COALESCE(num_60_79,0), COALESCE(num_50_59,0), COALESCE(num_0_49,0)
+FROM solution
+;
