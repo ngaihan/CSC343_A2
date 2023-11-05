fa68b07a1ce71eab4e58ce3388d19314b62129df
Ngai Han Lin <ngaihan@teach.cs.toronto.edu>
2023-11-05 09:07:53 -0500

    still working on q7

--- a2-starter-code/q7.sql
+++ a2-starter-code/q7.sql
@@ -10,12 +10,35 @@ CREATE TABLE q7 (
 -- Do this for each of the views that define your intermediate steps.
 -- (But give them better names!) The IF EXISTS avoids generating an error
 -- the first time this file is imported.
-DROP VIEW IF EXISTS intermediate_step CASCADE;
+DROP VIEW IF EXISTS atLeastOneGroup CASCADE;
 
 
 -- Define views for your intermediate steps here:
-CREATE VIEW intermediate_step AS ... ;
+-- Grader that has been assigned at least one group per assignment
+CREATE VIEW atLeastOneGroup AS
+SELECT username
+FROM grader JOIN assignmentgroup AS T1 USING (group_id)
+WHERE EXISTS (
+	SELECT *
+	FROM assignment AS T2
+	WHERE T1.assignment_id = T2.assignment_id
+)
+GROUP BY username
+;
+
+-- Grader that marked somemthing from every student
+CREATE VIEW notAllStudent AS
+SELECT grader.username
+FROM membership JOIN grader USING (group_id)
+WHERE EXISTS (
+	SELECT *
+	FROM MarkusUser
+	WHERE usertype = 'student' AND membership.username=markususer.username
+)
+;
+
+CREATE VIEW everyStudent AS 
 
 
 -- Your query that answers the question goes below the "insert into" line:
-INSERT INTO q7
+-- INSERT INTO q7
